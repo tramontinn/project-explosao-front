8860264d11eb07187f364261d8afb86101850aa8
Guilherme Vinicios Tramontin <gvtramontin@gmail.com>
2023-04-16 22:14:11 -0300

    Login page
    Add the login page

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import backgroud from '../images/Banner_Principal.png'
+import styles from './Login.module.css'
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div className={styles.page}>
+      <img src={backgroud} className={styles.img}></img>
+      <div className={styles.component}>Login</div>
+        <form className={styles.form}>
+          <div>
+            <input type="text" placeholder="Usuário" />
+          </div>
+          <div>
+            <input type="password" placeholder="Digite sua senha" />
+          </div>
+          <div>
+            <input type="submit" value="Entrar" />
+          </div>
+        </form>
+    </div>
   )
 }
 

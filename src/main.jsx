24fc354309645469e89d856bc373c883c4decbf5
Guilherme Vinicios Tramontin <gvtramontin@gmail.com>
2023-05-09 08:44:37 -0300

    Inicio autenticação API Login

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,21 @@ import Home from './views/index'
 import Login from './views/login'
 import Dashboard from './views/admin/dashboard'
 import PrivateRoute from './privateRoute'
-
+import { AuthContext } from './services/auth'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/admin">
-          <Route path="dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
-        </Route>
-      </Routes>
+      <AuthContext.Provider>
+        <Routes>
+          <Route index path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/admin">
+            <Route path="dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
+          </Route>
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>  
   </React.StrictMode>
 )
--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,18 +1,49 @@
+import { useState } from 'react'
+
 import React from 'react'
 import backgroud from '../images/banner_login.png'
 import logo from '../images/Logo.png'
 import styles from './Login.module.css'
+import api from '../services/api'
+
 
 const Login = () => {
+
+  
+
+  //setamos o useState para pegar os dados e armazenar nas variáveis "set"
+  const [user, setUser] = useState("")
+  const [senha, setSenha] = useState("")
+
+  //Faz com que o submit não recarregue a página e perca os dados
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('submitei', { user, senha })
+  }
+
   return (
     <div className={styles.page}>
       <img src={backgroud} className={styles.img}></img>
       <img src={logo} className={styles.logo}></img>
       <div className={styles.base}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.form}> 
-            <input type="text" placeholder="Usuário" />
-            <input type="password" placeholder="Senha" />
+            <input 
+              type="text" 
+              id='user' 
+              name='user' 
+              placeholder="Usuário" 
+              value={user} 
+              onChange={(e) => setUser(e.target.value)}/>
+            <input 
+              type="password" 
+              placeholder="Senha" 
+              id='password' 
+              name='password' 
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}  
+            />
             <input className={styles.button} type="submit" value="Entrar" />
           </div>
         </form>
